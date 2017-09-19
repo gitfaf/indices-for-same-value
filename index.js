7136d987eaa75fb167d600f5ed98039c34d79237
Git Faf <git.faf@gmail.com>
2017-09-19 17:41:51 +0530

    Working code

---
+++ index.js
@@ -0,0 +1,9 @@
+function getAllIndices(forValue, inArray) {
+    var indices = []
+    for (let index of Object.keys(inArray)) {
+        if (inArray[index] === forValue) {
+            indices.push(index)
+        }
+    }
+    return indices
+}
